c3ffa4ca3b40747eab791f00f0061fc56b040ab4
Ymir <ymir789@gmail.com>
2026-03-20 17:03:49 +0900

    fix: 기인 게이지 HUD 연결
    로컬변수인 기인게이지(SpiritGuage)는 따로 Set함수를 만들고 그 안에 Broadcast 하도록 수정
    디버깅용 플레이어에게 대미지 입히는 Exec 함수 추가

--- Source/ProjectMHW/Private/Character/Player/MHPlayerCharacter.cpp
+++ Source/ProjectMHW/Private/Character/Player/MHPlayerCharacter.cpp
@@ -1265,6 +1265,28 @@ float AMHPlayerCharacter::GetCurrentSpiritDamageMultiplier() const
     }
 }
 
+void AMHPlayerCharacter::SetSpiritGaugeValues(const float InSpiritValue, const float InMaxSpiritValue)
+{
+    MaxSpiritGauge = FMath::Max(0.f, InMaxSpiritValue);
+    CurrentSpiritGauge = FMath::Clamp(InSpiritValue, 0.0f, MaxSpiritGauge);
+    
+    OnSpiritGaugeChanged.Broadcast(GetCurrentSpiritGaugeValue(), GetMaxSpiritGaugeValue());
+}
+
+void AMHPlayerCharacter::SetCurrentSpiritGauge(const float InSpiritValue)
+{
+    CurrentSpiritGauge = FMath::Clamp(InSpiritValue, 0.0f, MaxSpiritGauge);
+    
+    OnSpiritGaugeChanged.Broadcast(GetCurrentSpiritGaugeValue(), GetMaxSpiritGaugeValue());
+}
+
+void AMHPlayerCharacter::SetMaxSpiritGuage(const float InMaxSpiritValue)
+{
+    MaxSpiritGauge = FMath::Max(0.f, InMaxSpiritValue);
+    
+    OnSpiritGaugeChanged.Broadcast(GetCurrentSpiritGaugeValue(), GetMaxSpiritGaugeValue());
+}
+
 float AMHPlayerCharacter::ResolveLongSwordDamageMultiplier(const FGameplayTag& InMoveTag) const
 {
     FMHAttackMetaRow AttackMetaRow;
@@ -1374,7 +1396,8 @@ void AMHPlayerCharacter::AddSpiritGauge(const float InAmount)
         return;
     }
 
-    CurrentSpiritGauge = FMath::Clamp(CurrentSpiritGauge + InAmount, 0.0f, FMath::Max(0.0f, MaxSpiritGauge));
+    const float NewSpiritGauge = FMath::Clamp(CurrentSpiritGauge + InAmount, 0.0f, FMath::Max(0.0f, MaxSpiritGauge));
+    SetCurrentSpiritGauge(NewSpiritGauge);
 }
 
 void AMHPlayerCharacter::ConsumeSpiritGauge(const float InAmount)
@@ -1384,7 +1407,8 @@ void AMHPlayerCharacter::ConsumeSpiritGauge(const float InAmount)
         return;
     }
 
-    CurrentSpiritGauge = FMath::Clamp(CurrentSpiritGauge - InAmount, 0.0f, FMath::Max(0.0f, MaxSpiritGauge));
+    const float NewSpiritGauge = FMath::Clamp(CurrentSpiritGauge - InAmount, 0.0f, FMath::Max(0.0f, MaxSpiritGauge));
+    SetCurrentSpiritGauge(NewSpiritGauge);
 }
 
 void AMHPlayerCharacter::IncreaseSpiritLevel(const int32 InAmount)
@@ -2982,16 +3006,18 @@ void AMHPlayerCharacter::BindAttributeDelegates()
     ).AddUObject(this, &ThisClass::HandleHealthAttributeChanged);
 
     AbilitySystemComponent->GetGameplayAttributeValueChangeDelegate(
-        UMHHealthAttributeSet::GetMaxHealthAttribute()
-    ).AddUObject(this, &ThisClass::HandleMaxHealthAttributeChanged);
-
-    AbilitySystemComponent->GetGameplayAttributeValueChangeDelegate(
         UMHPlayerAttributeSet::GetStaminaAttribute()
     ).AddUObject(this, &ThisClass::HandleStaminaAttributeChanged);
-
-    AbilitySystemComponent->GetGameplayAttributeValueChangeDelegate(
-        UMHPlayerAttributeSet::GetMaxStaminaAttribute()
-    ).AddUObject(this, &ThisClass::HandleMaxStaminaAttributeChanged);
+    
+    //TODO: 예리도 기인 게이지  
+    
+    // AbilitySystemComponent->GetGameplayAttributeValueChangeDelegate(
+    //     UMHHealthAttributeSet::GetMaxHealthAttribute()
+    // ).AddUObject(this, &ThisClass::HandleMaxHealthAttributeChanged);
+    //
+    // AbilitySystemComponent->GetGameplayAttributeValueChangeDelegate(
+    //     UMHPlayerAttributeSet::GetMaxStaminaAttribute()
+    // ).AddUObject(this, &ThisClass::HandleMaxStaminaAttributeChanged);
 
     bAttributeDelegatesBound = true;
 }
