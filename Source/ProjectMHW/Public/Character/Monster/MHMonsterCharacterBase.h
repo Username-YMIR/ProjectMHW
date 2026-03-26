ab0db8d19ebb03f3b5be899908bfeeb366ae36ac
somnis23 <somnis0422@gmail.com>
2026-03-26 15:20:59 +0900

    feat : 몬스터 sfx ,vfx  추가  _ 허혁

--- Source/ProjectMHW/Public/Character/Monster/MHMonsterCharacterBase.h
+++ Source/ProjectMHW/Public/Character/Monster/MHMonsterCharacterBase.h
@@ -34,6 +34,7 @@ public:
 
 protected:
     virtual void BeginPlay() override;
+    virtual void Landed(const FHitResult& Hit) override;
 
 public:
     UFUNCTION(BlueprintPure , Category="Monster|Combat")
@@ -125,7 +126,7 @@ public:
     float ChargeP2JumpZVelocity = 1000.f;
 
     UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Monster|ChargeP2")
-    float ChargeP2AirTime = 0.1f;
+    float ChargeP2AirTime = 0.6f;
 
     UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Monster|ChargeP2")
     float ChargeP2MinXYLaunchSpeed = 100.f;
@@ -139,6 +140,24 @@ public:
     UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Monster|ChargeP2")
     bool bChargeP2LandingPrepared = false;
 
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Monster|ChargeP2")
+    bool bChargeP2InAir = false;
+
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Monster|ChargeP2")
+    float ChargeP2ImpactBlendDuration = 0.1f;
+
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Monster|ChargeP2")
+    bool bChargeP2ImpactBlending = false;
+
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Monster|ChargeP2")
+    FVector ChargeP2ImpactBlendStartLocation = FVector::ZeroVector;
+
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Monster|ChargeP2")
+    float ChargeP2ImpactBlendElapsed = 0.f;
+
+    UFUNCTION(BlueprintPure, Category="Monster|ChargeP2")
+    bool IsChargeP2InAir() const { return bChargeP2InAir; }
+
     UFUNCTION(BlueprintCallable, Category="Monster|ChargeP2")
     void PrepareChargeP2Landing();
 
@@ -150,6 +169,13 @@ public:
 
     UFUNCTION(BlueprintCallable, Category="Monster|ChargeP2")
     void ClearChargeP2Landing();
+
+protected:
+    void StartChargeP2ImpactBlend();
+    void UpdateChargeP2ImpactBlend();
+    void FinishChargeP2ImpactBlend();
+
+    FTimerHandle ChargeP2ImpactBlendTimer;
     
     
 #pragma endregion
